bcaa522713a895875d27a435b71d4b60b82d6a78
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2023-12-30 17:59:47 -0500

    Database added

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
+const appSettings = {
+	databaseURL: "https://listify-e794f-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+
 let addBtn = document.querySelector('#addBtn');
 let userInput = document.querySelector('#userInput')
 
