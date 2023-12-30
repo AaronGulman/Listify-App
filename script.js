498b43e23631bf5d26c72e14cfff17c95b133468
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2023-12-30 17:58:05 -0500

    Import initializeApp

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
 let addBtn = document.querySelector('#addBtn');
 let userInput = document.querySelector('#userInput')
 
