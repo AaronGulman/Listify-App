176e2d9326c9fa914961e0a2de84fcd4ee0ff003
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2023-12-30 20:14:48 -0500

    Add li whenever the button is clicked

--- script.js
+++ script.js
@@ -10,13 +10,21 @@ const database = getDatabase(app);
 const itemsInDB = ref(database, 'items')
 
 
+
+
 let addBtn = document.querySelector('#addBtn');
 let userInput = document.querySelector('#userInput');
+let uList = document.getElementById('ulist')
+
+
 
 addBtn.addEventListener('click', newItem);
 
 function newItem(){
 	let inputValue = userInput.value;
 	push(itemsInDB,inputValue);
+	let list = document.createElement('li')
+	uList.appendChild(list)
+	list.textContent += inputValue;
 	console.log(`${inputValue},added to database`);
 }
