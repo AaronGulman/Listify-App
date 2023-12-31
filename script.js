49c23b2e88bde99200f617f2eb8ad198bd652d81
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2023-12-30 20:06:07 -0500

    Database access

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
 	databaseURL: "https://listify-e794f-default-rtdb.firebaseio.com/"
 }
 
-const app = initializeApp(appSettings)
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const itemsInDB = ref(database, 'items')
+
 
 let addBtn = document.querySelector('#addBtn');
-let userInput = document.querySelector('#userInput')
+let userInput = document.querySelector('#userInput');
 
-addBtn.addEventListener('click', newItem)
+addBtn.addEventListener('click', newItem);
 
 function newItem(){
-	console.log(userInput.value)
+	let inputValue = userInput.value;
+	push(itemsInDB,inputValue);
+	console.log(`${inputValue},added to database`);
 }
